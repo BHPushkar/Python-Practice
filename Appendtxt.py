e11281ffe1018887140667e5b53803a5f62739e8
Pushkar Mangesh Bhalerao <97427069+BHPushkar@users.noreply.github.com>
2022-01-17 13:57:47 +0530

    Add files via upload

---
+++ Appendtxt.py
@@ -0,0 +1,4 @@
+f=open("sample2.txt","a")           # It will make the file if not existed
+a=f.write("Welcome to sample txt file 02")
+print(a)                             # It will print the number of index in output box
+f.close()
